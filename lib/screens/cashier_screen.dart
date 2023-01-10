68ddff6e08be8b4c13e19e401f59989dca229eb8
Syifa Saif Hidayat <syifa.sh404@students.amikom.ac.id>
2023-01-11 00:16:31 +0700

    first commit

---
+++ lib/screens/cashier_screen.dart
@@ -0,0 +1,194 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dashboard/theme.dart';
+import 'package:flutter/material.dart';
+
+class CashierScreen extends StatelessWidget {
+  const CashierScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: const Text(
+          "Kasir",
+          style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        actions: const [],
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+          stream:
+              FirebaseFirestore.instance.collection("pengunjung").snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              var items = snapshot.data;
+              print(items!.docs[items.docs.length - 1]["value"]);
+              return Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                decoration: const BoxDecoration(color: Colors.green),
+                padding: EdgeInsets.fromLTRB(
+                    20, MediaQuery.of(context).size.height * 0.15, 20, 0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        boxRectangle(
+                            "Hari ini",
+                            items.docs[items.docs.length - 1]["value"]
+                                .toString()),
+                        boxRectangle(
+                            "Kemarin",
+                            items.docs[items.docs.length - 2]["value"]
+                                .toString())
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 30.0,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                            child: boxRectangle(
+                                "Pengunjung Jam ${DateTime.now().hour > 20 || DateTime.now().hour < 7 ? "" : DateTime.now().hour}",
+                                items.docs[items.docs.length - 1]
+                                        .toString()
+                                        .contains(
+                                            "pengunjung_jam_${DateTime.now().hour}")
+                                    ? items.docs[items.docs.length - 1][
+                                            "pengunjung_jam_${DateTime.now().hour}"]
+                                        .toString()
+                                    : "0",
+                                height: 100)),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 20),
+                      child: Text(
+                        "Data",
+                        style: whiteTextStyle.copyWith(
+                            fontSize: 36, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 20.0,
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.40,
+                            child: SingleChildScrollView(
+                              child: Column(
+                                children: [
+                                  Column(
+                                    children: [
+                                      detailsContainer(),
+                                      detailsContainer(),
+                                      detailsContainer(),
+                                      detailsContainer(),
+                                      detailsContainer(),
+                                      detailsContainer(),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+          }),
+    );
+  }
+
+  Container detailsContainer() {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 15),
+      decoration: BoxDecoration(
+          color: Colors.green, borderRadius: BorderRadius.circular(13)),
+      height: 65,
+      padding: const EdgeInsets.fromLTRB(20, 5, 10, 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "Dewa",
+                style: whiteTextStyle.copyWith(
+                  fontSize: 20.0,
+                ),
+              ),
+              Row(
+                children: [
+                  const SizedBox(
+                    width: 20.0,
+                  ),
+                  Text(
+                    "Rp134.000",
+                    style: whiteTextStyle.copyWith(
+                      fontSize: 18,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container boxRectangle(String title, String body, {double height = 150}) {
+    return Container(
+      height: height,
+      width: 150,
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(25)),
+      padding: const EdgeInsets.only(top: 10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: blackTextStyle.copyWith(
+              fontSize: 24,
+            ),
+          ),
+          Text(
+            body,
+            style: blackTextStyle.copyWith(
+              fontSize: 40,
+            ),
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+        ],
+      ),
+    );
+  }
+}
